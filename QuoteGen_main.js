aeb0539a9fc8ee5687ea4ba025549f15d85d2cb1
coding-engineer-1701 <alexander.g.bush@gmail.com>
2025-02-25 07:58:05 +1000

    Initial plan for quotegen script.

---
+++ QuoteGen_main.js
@@ -0,0 +1,26 @@
+console.log("Hello, world!")
+
+// Create a Function that generates Quotes
+function generateQuote() {
+    // gets a quote from quote list or library
+    // Returns quote and author as formatted string
+}
+
+
+
+
+// create a quotefactory function that will create a quote object
+// Create a quote object that contains methods for storing and returning quotes
+`
+methods
+ - change quote - will update the quote
+ - print quote - will create the formatted quote
+`
+
+
+// Create a library of quotes with Quote and Author?
+`
+methods
+ - get quote
+ - add quote - 
+`

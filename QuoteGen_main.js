fbb8d30e727d09473390a161891dff89206a088f
coding-engineer-1701 <alexander.g.bush@gmail.com>
2025-02-25 12:58:22 +1000

    Started creating quoteFactory and quoteLibrary objects

--- QuoteGen_main.js
+++ QuoteGen_main.js
@@ -16,11 +16,38 @@ methods
  - change quote - will update the quote
  - print quote - will create the formatted quote
 `
+const quoteFactory = (Quote, Author='', Date='') => {
+    if (Author === '') {
+        let author_val = 'Unknown';
+    } else {
+        let author_val = Author;
+    }
+    if (Date === '') {
+        let date_val = 'Unknown';
+    } else {
+        let date_val = Date;
+    }
+
+    return {
+        quote: Quote,
+        author: author_val,
+        date: date_val,
+        print_quote() {
+            // put the quote return in here
+        }
+    }
+
+}
 
 
 // Create a library of quotes with Quote and Author?
 `
 methods
+ - load the quotes list from memory
  - get quote
  - add quote - 
 `
+
+const QuoteLibrary = {
+
+}
